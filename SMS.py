3f526d945b2554171de7c6a8066f9a1727663131
Aayush <224820692+codewithaayush-lang@users.noreply.github.com>
2026-07-31 23:46:02 +0530

    Added StudentManagement class

--- SMS.py
+++ SMS.py
@@ -14,13 +14,50 @@ class Student:
         self.marks = newmarks
 
     def pass_fail(self):
-        if self.marks>30:
-            print("PASS")
+        if self.marks>=30:
+            print(f"{self.name} has passed!")
         else:
-            print("FAIL")
+            print(f"{self.name} has failed")
 
-student1 = Student("Aayush",23,78)
+class StudentManagement:
+    def __init__(self):
+        self.students = []
 
-student1.display_details()
-student1.update_marks(30)
-student1.pass_fail()
+    def add_student(self,student):
+        self.students.append(student)
+
+    def display_all_students(self):
+        for student in self.students:
+            student.display_details()
+
+    def search_student(self,roll):
+        for student in self.students:
+            if roll == student.roll:
+                student.display_details()
+                return
+        print("No student found with that name!")
+
+    def remove_student(self,roll):
+        for student in self.students:
+            if roll == student.roll:
+                self.students.remove(student)
+                print("Student removed successfully")
+                return
+        print("No student found with that roll number!")
+
+
+#testing
+
+management1 = StudentManagement()
+
+s1 = Student("Aayush",146,90)
+s2 = Student("Rahul",147,85)
+
+management1.add_student(s1)
+management1.add_student(s2)
+
+management1.display_all_students()
+
+management1.remove_student(146)
+
+management1.display_all_students()
