aa2af733aed4f42141753990485279a24501858b
Aayush <224820692+codewithaayush-lang@users.noreply.github.com>
2026-08-01 11:21:37 +0530

    Created command line menu

--- SMS.py
+++ SMS.py
@@ -9,6 +9,7 @@ class Student:
         print(f"Name: {self.name}")
         print(f"Roll No. {self.roll}")
         print(f"Marks: {self.marks}")
+        self.pass_fail()
 
     def update_marks(self,newmarks):
         self.marks = newmarks
@@ -25,8 +26,13 @@ class StudentManagement:
 
     def add_student(self,student):
         self.students.append(student)
+        print("Student added successfully")
 
     def display_all_students(self):
+        if not self.students:
+            print("No students available")
+            return
+        
         for student in self.students:
             student.display_details()
 
@@ -35,7 +41,7 @@ class StudentManagement:
             if roll == student.roll:
                 student.display_details()
                 return
-        print("No student found with that name!")
+        print("No student found with that roll no.!")
 
     def remove_student(self,roll):
         for student in self.students:
@@ -46,18 +52,54 @@ class StudentManagement:
         print("No student found with that roll number!")
 
 
-#testing
+#remove the testing portion if you dont want to test the Student Management List
 
-management1 = StudentManagement()
+# testing portion starts from here
 
-s1 = Student("Aayush",146,90)
-s2 = Student("Rahul",147,85)
+# management1 = StudentManagement()
 
-management1.add_student(s1)
-management1.add_student(s2)
+# s1 = Student("Aayush",146,90)
+# s2 = Student("Rahul",147,85)
 
-management1.display_all_students()
+# management1.add_student(s1)
+# management1.add_student(s2)
 
-management1.remove_student(146)
+# management1.display_all_students()
 
-management1.display_all_students()
+# management1.remove_student(146)
+
+# management1.display_all_students()
+
+#testing portion ends here
+
+# Menu driven interface
+
+management = StudentManagement()
+
+while True:
+    print("\n===== Student Management System =====")
+    print("1. Add Student")
+    print("2. Display All Students")
+    print("3. Search Student")
+    print("4. Remove Student")
+    print("5. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        name = input("Enter the name of the student:")
+        roll = int(input("Rollno. of the student:"))
+        marks = int(input("Marks:"))
+        student = Student(name,roll,marks)
+        management.add_student(student)
+    elif choice == "2":
+        management.display_all_students()
+    elif choice == "3":
+        roll = int(input("Enter the roll no. of the student: "))
+        management.search_student(roll)
+    elif choice == "4":
+        roll = int(input("Enter the roll nol of the student you want to remove: "))
+        management.remove_student(roll)
+    elif choice == "5":
+        break
+    else:
+        print("Invalid menu choice! Please try again")
+        
